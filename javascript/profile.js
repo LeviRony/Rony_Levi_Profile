f152b61dd686528e6f442e2b48f3fe8b9fad3c3f
RonyBoom <ronylevi99@gmail.com>
2025-03-10 21:33:59 +0200

    js action for contact buttom

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,3 +1,9 @@
 function onClickLinkeDin(event){
     document.getElementsByClassName('fa fa-linkedin');
 }
+
+function clearTextarea() {
+    document.getElementById("message").value = "";
+    location.reload();
+    alert("Thank you")
+}
