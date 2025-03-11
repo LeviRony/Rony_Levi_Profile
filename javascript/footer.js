c4711878d230e5c3f9f4aa246a408b8b13b7fee1
RonyBoom <ronylevi99@gmail.com>
2025-03-11 16:48:24 +0200

    fixing footer and update files names

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/footer.html")
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("footer.html") // Adjust path if needed
     .then(response => response.text())
     .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-    });
+      document.body.insertAdjacentHTML("beforeend", data);
+    })
+    .catch(error => console.error("Error loading footer:", error));
 });
