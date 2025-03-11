3643b0d8cacd63dbb438efbc426d8afe73d62385
RonyBoom <ronylevi99@gmail.com>
2025-03-11 21:16:42 +0200

    reformat

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -5,8 +5,8 @@ const items = document.querySelectorAll('.scattered-item');
 const container = document.getElementById('gridContainer');
 
 items.forEach(item => {
-  const randomTop = Math.random() * (container.offsetHeight - 150); // Random position within container height
-  const randomLeft = Math.random() * (container.offsetWidth - 150); // Random position within container width
-  item.style.top = `${randomTop}px`;
-  item.style.left = `${randomLeft}px`;
+    const randomTop = Math.random() * (container.offsetHeight - 150); // Random position within container height
+    const randomLeft = Math.random() * (container.offsetWidth - 150); // Random position within container width
+    item.style.top = `${randomTop}px`;
+    item.style.left = `${randomLeft}px`;
 });
